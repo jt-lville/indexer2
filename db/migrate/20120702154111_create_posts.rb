8950e4e17f9da61afce8137a66154a508ba131b2
Jonathan Tang <kyothine@gmail.com>
2012-07-02 11:42:51 -0400

    adding posts

---
+++ db/migrate/20120702154111_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      
+      t.string :name
+      t.text :description
+      t.string :location
+      t.decimal :cost
+      t.decimal :size
+      t.string :category #e.g. electronics, books, etc
+      t.string :owner
+      
+      t.timestamps
+    end
+  end
+end
