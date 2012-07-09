1ab3e859e8f1faa08ac265c0d8ddb2d7c329c368
Jonathan Tang <kyothine@gmail.com>
2012-07-09 14:44:17 -0400

    prettifying

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
 
 	attr_accessible :name, :description, :location, :cost, :size, :url, :weight, :category, :owner
 	validates :name, :presence => true
+	validates :category, :presence => true
+	validates :location, :presence => true
 
 	belongs_to :user
 
